38d4a23dfe716edc2904c77a8488661252ec8e82
sujiny3236lovegit <69749672+sujiny3236lovegit@users.noreply.github.com>
2020-12-22 22:13:18 +0900

    stage

--- README.md
+++
@@ -1,2 +0,0 @@
-# Interactive-web
- Interactive-web
---
+++ js/wall3d.js
@@ -0,0 +1,29 @@
+(function(){
+  const stageElem = document.querySelector('.stage');
+  const houseElem = document.querySelector('.house');
+  const barElem = document.querySelector('.progress-bar');
+  const mousePos = { x: 0, y: 0 };
+  let maxScrollValue;
+  
+  function resizeHandler(){
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  };
+
+  window.addEventListener('scroll', function(){
+    const scrollPer = pageYOffset / maxScrollValue; 
+    const zMove = scrollPer * 980 - 490;
+    houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+
+    // progress bar
+    barElem.style.width = scrollPer * 100 + '%';
+  });
+
+  window.addEventListener('mousemove', function(e){
+    // console.log(e.clientX, e.clientY);
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) + 'deg) rotateY(' + (mousePos.x * 5) + 'deg)'; // 회전
+  });
+  window.addEventListener('resize', resizeHandler);
+  resizeHandler();
+})();
